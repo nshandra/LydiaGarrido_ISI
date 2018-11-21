ba9194a3e2278f1075790e1ad265bffd246f7a7e
Adrián Payol Montero <35956442+apayol@users.noreply.github.com>
2018-11-21 20:46:12 +0100

    Añadidos test embotelladora

--- Embotelladora/EmbotelladoraTest.java
+++ Embotelladora/EmbotelladoraTest.java
@@ -5,29 +5,58 @@ import org.junit.runners.Parameterized.Parameters;
 import static org.junit.Assert.*;
 import java.util.*;
 
-@RunWith (Parameterized.class)
 public class EmbotelladoraTest
 {
+	Embotelladora embotelladora = new Embotelladora();
 	public int pequenas, grandes, total;
 
-	public EmbotelladoraTest (int pequenas, int grandes, int total)
+	//Testing b1: grandes, pequenas o total son negativos
+	@Test (expected = RuntimeException.class)
+	public void negativeTest()
 	{
-		this.pequenas = pequenas;
-		this.grandes = grandes;
-		this.total = total;
+		pequenas = -1;
+		grandes = 0;
+		total = 3;
+		embotelladora.calculaBotellasPequenas(pequenas, grandes, total);
 	}
 	
-	@Parameters
-	public static Collection<Object[]> embotelladoraValues()
+	//Testing b2: total > miCapacidad
+	@Test (expected = RuntimeException.class)
+	public void b2Test()
 	{
-		return Arrays.asList (new Object [][] {{0, 0, 0}, {10,2,40}});
+		pequenas = 4;
+		grandes = 1;
+		total = 20;
+		embotelladora.calculaBotellasPequenas(pequenas, grandes, total);
 	}
-
+	
+	//Testing b3: total <= miCapacidad y total-5*grandes<0
 	@Test
-	public void enoughTest()
+	public void b3Test()
 	{
-		assertTrue ("Enough Test", total - (grandes*5) >= pequenas);
+		pequenas = 2;
+		grandes = 5;
+		total = 5;
+		assertTrue ("b3Test", embotelladora.calculaBotellasPequenas(pequenas, grandes, total) == 0);
+	}
+	
+	//Testing b4: total <= miCapacidad y total-5*grandes>0
+	@Test
+	public void b4Test()
+	{
+		pequenas = 3;
+		grandes = 1;
+		total = 7;
+		assertTrue ("b4Test", embotelladora.calculaBotellasPequenas(pequenas, grandes, total) == 2);
+	}
+	
+	//Testing b5: total <= miCapacidad y total-5*grandes=0
+	@Test
+	public void b5Test()
+	{
+		pequenas = 0;
+		grandes = 4;
+		total = 20;
+		assertTrue ("b5Test", embotelladora.calculaBotellasPequenas(pequenas, grandes, total) == 0);
 	}
 }
-		
-  
