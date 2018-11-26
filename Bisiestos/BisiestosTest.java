fd34cbeeb854f02c94da36d3a8e2a6d5c7f5ec24
jaimefdez96 <jfs_x@hotmail.com>
2018-11-26 19:38:41 +0100

    Mi test modificado, explicación en el txt añadido

--- Bisiestos/BisiestosTest.java
+++ Bisiestos/BisiestosTest.java
@@ -1,59 +1,56 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 import java.util.*;
-import org.junit.runner.RunWith;
-import static org.junit.Assume.*;
 
+public class BisiestosTest {
+	/* 5 Test:
+	--> 1) Happy path
+	--> 2) Año negativo
+	--> 3) No negativo divisible entre 4 pero no entre 100 ni 400
+	--> 4) No negativo divisible entre 100, pero no entre 4 ni 400
+	--> 5) No negativo divisible entre 4 y 100 pero no entre 400
+	*/
+	private int year;
+	Bisiestos bis = new Bisiestos();
 
-public class BisiestosTest
-{
-   
-	//Test para comprobar años inválidos.
-
-   	@Test (expected = RuntimeException.class)
-   	public void isLeap()
-	{
-		int year = 0;
-		Bisiestos.esBisiesto(year);
+	//Test 1
+	@Test
+	public void testHappyPath(){
+		year = 2000;
+		assertTrue("Es bisiesto, fallo", true == bis.esBisiesto(year));
+		System.out.println(year + " es bisiesto");
 	}
 
+	//Test 2
+	//@Test (expected = IllegalArgumentException.class)
+	//---> Voy a modificar mi test
 	@Test (expected = RuntimeException.class)
-   	public void isLeap2()
-	{
-		int year = -1;
-		Bisiestos.esBisiesto(year);
+	public void testNegativeYear() {
+		year = -2000;
+		bis.esBisiesto(year);
 	}
 
-	//Año divisible entre 400
-	
+	//Test 3
 	@Test
-	public void isLeap3()
-	{
-		int year = 400;
-		boolean result = Bisiestos.esBisiesto(year);
-		assertTrue(result);
+	public void testDivisibleBy4(){
+		year = 1904;
+		assertTrue("Es bisiesto, fallo", true == bis.esBisiesto(year));
+		System.out.println(year + " es bisiesto");
 	}
-	
 
-	//Año no divisible entre 4, 100 y 400
-
+	//Test 4
 	@Test
-	public void isLeap4()
-	{
-		int year = 101;
-		boolean result = Bisiestos.esBisiesto(year);
-		assertFalse(result);
+	public void testDivisibleBy100(){
+		year = 700;
+		assertTrue("No bisiesto, fallo", false == bis.esBisiesto(year));
+		System.out.println(year + " no es bisiesto");
 	}
-	
-	//Año no bisiesto
 
+	//Test 5
 	@Test
-	public void isLeap5()
-	{
-		int year = 1;
-		boolean result = Bisiestos.esBisiesto(year);
-		assertTrue(result);
+	public void testDivisibleBy4And100(){
+		year = 2200;
+		assertTrue("No es bisiesto, fallo", false == bis.esBisiesto(year));
+		System.out.println(year + " no es bisiesto");
 	}
-	
-  }
-   
+}
